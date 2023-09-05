7cc4c59ab52fb2d2df00a9a40ae15970dd3bfeaf
mand8ate <mand8ate@gmail.com>
2023-09-05 17:09:04 +0900

    change to utils

--- utils/index.ts
+++ utils/index.ts
@@ -33,7 +33,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
   return rentalRatePerDay.toFixed(0);
 };
 
-export const generateCarImageUrl = (car: CarProps, angle: string) => {
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
   const url = new URL("https://cdn.imagin.studio/getimage");
 
   const { make, year, model } = car;
